947e07129d6e8fbf7b397f67bdb825867bd87daf
adrianix2 <adrianpietruszka98@gmail.com>
2018-03-30 21:36:39 +0200

    Program_Calki

--- calki.c
+++ calki.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
+#define n 100000;
 
 
 double fun1(double x)
@@ -19,67 +20,36 @@ double fun3(double x)
 	return sin(x);
 }
 
-double prostakaty(double a, double b, int i)
+double fun4(double x)
+{
+	return x*x + 2*x - 1;
+}
+
+double prostakaty(double (*fun)(double), double a, double b)
 {
 	double krok;
-	krok = fabs((b-a))/100000;
+	krok = fabs((b-a))/n;
 	double calka, x = a;
 	calka = 0;
-	if(i == 1)
+	while(b > x)
 	{
-		while(b > x)
-		{
-			calka+= krok*fun1(x);
-			x += krok;
-		}
-	}	
-	else if(i == 2)
-	{
-		while(b > x)
-		{
-			calka+= krok*fun2(x);
-			x += krok;
-		}
-	}	
-	else
-	{
-		while(b > x)
-		{
-			calka+= krok*fun3(x);
-			x += krok;
-		}
-	}	
+		calka+= krok*fun(x);
+		x += krok;
+	}
 	return calka;
 
 }
 
-double trapezy(double a, double b, int i)
+double trapezy(double (*fun)(double), double a, double b)
 {
 	
 	double krok;
-	krok = fabs((b-a))/100000;
-	double calka, x = a;
-	
-	if(i == 1)
-	{
-		calka = 0.5 * x * (fun1(a) + fun1(b));
-		for(x = a + krok; x < b; x += krok)
-			calka += fun1(x);
-	}
-	else if(i == 2)
-	{
-		calka = 0.5 * x * (fun2(a) + fun2(b));
-		for(x = a + krok; x < b; x += krok)
-			calka += fun2(x);
-	}
-	else
-	{
-		calka = 0.5 * x * (fun3(a) + fun3(b));
-		for(x = a + krok; x < b; x += krok)
-			calka += fun3(x);
-	}
-		calka *= krok;
-	
+	krok = fabs((b-a))/n;
+	double calka = 0, x;
+	calka = 0.5 * a * (fun(a) + fun(b));
+	for(x = a + krok; x < b; x += krok)
+		calka += fun(x);
+	calka *= krok;
 	return calka;
 }
 
@@ -92,80 +62,34 @@ double losuj(double x, double y)
 	return a;
 }
 
-double  monte_carlo(double a, double b, int i)
+double  monte_carlo(double (*fun)(double), double a, double b)
 {
 	
 	double krok;
-	krok = fabs((b-a))/100000;
+	krok = fabs((b-a))/n;
 	double MIN = 0, MAX = 0;
 	double x, y;
-	double ff;
 	int N = 0;
-	if(i == 1)
-	{
-		for(x = a; x <= b; x += krok)
-		{	
-			if(fun1(x)  > MAX)
-				MAX = fun1(x);
-			else if(fun1(x) < MIN)
-				MIN = fun1(x);
-		}	
-	
-		for(int i = 0; i < 1000000; i++)
-		{	
-			x = losuj(a, b);
-			y = losuj(MIN, MAX);
-			if(y > 0)
-				if(fun1(x) > y)
-					N++;
-			if (y < 0)
-				if(fun1(x) < y)
-					N--;
-		}	
-	}
-	
-	else if(i == 2)
-	{
-		for(x = a; x <= b; x += krok)
-		{	
-			if(fun2(x)  > MAX)
-				MAX = fun2(x);
-			else if(fun2(x) < MIN)
-				MIN = fun2(x);
-		}	
-		for(int i = 0; i < 1000000; i++)
-		{	
-			x = losuj(a, b);
-			y = losuj(MIN, MAX);
-			if(y > 0)			
-				if(fun2(x) > y)
-					N++;
-			if (y < 0)
-				if(fun2(x) < y)
-					N--;
-		}	
-	}
-	else
-	{
-		for(x = a; x <= b; x += krok)
-		{	
-			if(fun3(x)  > MAX)
-				MAX = fun3(x);
-			else if(fun3(x) < MIN)
-				MIN = fun3(x);
-		}	
-		for(int i = 0; i < 1000000; i++)
-		{	
-			x = losuj(a, b);
-			y = losuj(MIN, MAX);
-			if(y > 0)
-				if(fun3(x) > y)
-					N++;
-			if (y < 0)
-				if(fun3(x) < y)
-				N--;
-		}
+	for(x = a; x <= b; x += krok)
+	{	
+		if(fun(x)  > MAX)
+			MAX = fun(x);
+		else if(fun(x) < MIN)
+			MIN = fun(x);
 	}	
+
+	for(int i = 0; i < 1000000; i++)
+	{	
+		x = losuj(a, b);
+		y = losuj(MIN, MAX);
+		if(y > 0)
+			if(fun(x) > y)
+				N++;
+		if (y < 0)
+			if(fun(x) < y)
+				N--;
+	}	
+
 	return N/1000000. * (b - a) * (MAX - MIN); 
 }
 
@@ -174,20 +98,24 @@ double  monte_carlo(double a, double b, int i)
 
 
 int main(int argc, char *argv[]){
-srand(time(NULL));
-double calka, a, b, krok;
-a = -1, b = 1;
-for(int i = 1; i <= 3; i++)
-{
-	{
-		printf("Metoda prostokatow w przedziale od %lf do %lf  calka = %lf\n", a, b, prostakaty(a, b, i));
-		printf("Metoda trapezow w przedziale od %lf  do %lf  calka = %lf\n",a, b, trapezy(a, b, i));
-		printf("Metoda monte carlo  w przedziale od %lf  do %lf  calka = %lf\n\n",a, b,  monte_carlo(a, b , i));
-	}
-	a += 2;
-	b += 3;
-}
-return 0;
-
+	srand(time(NULL));
+	double calka, a, b, krok;
+	a = -10, b = 1;
+	printf("Metoda prostokatow w przedziale od %lf do %lf  calka = %lf\n", a, b, prostakaty(&fun1,a, b));
+	printf("Metoda trapezow w przedziale od %lf  do %lf  calka = %lf\n",a, b, trapezy(&fun1, a, b));
+	printf("Metoda monte carlo  w przedziale od %lf  do %lf  calka = %lf\n\n",a, b,  monte_carlo(&fun1, a, b));
+	a = 2, b = 4;
+	printf("Metoda prostokatow w przedziale od %lf do %lf  calka = %lf\n", a, b, prostakaty(&fun2,a, b));
+	printf("Metoda trapezow w przedziale od %lf  do %lf  calka = %lf\n",a, b, trapezy(&fun2, a, b));
+	printf("Metoda monte carlo  w przedziale od %lf  do %lf  calka = %lf\n\n",a, b,  monte_carlo(&fun2, a, b));
+	a = 1, b = 2;
+	printf("Metoda prostokatow w przedziale od %lf do %lf  calka = %lf\n", a, b, prostakaty(&fun3,a, b));
+	printf("Metoda trapezow w przedziale od %lf  do %lf  calka = %lf\n",a, b, trapezy(&fun3, a, b));
+	printf("Metoda monte carlo  w przedziale od %lf  do %lf  calka = %lf\n\n",a, b,  monte_carlo(&fun3, a, b));
+	a = -4, b = -2;
+	printf("Metoda prostokatow w przedziale od %lf do %lf  calka = %lf\n", a, b, prostakaty(&fun4,a, b));
+	printf("Metoda trapezow w przedziale od %lf  do %lf  calka = %lf\n",a, b, trapezy(&fun4, a, b));
+	printf("Metoda monte carlo  w przedziale od %lf  do %lf  calka = %lf\n\n",a, b,  monte_carlo(&fun4, a, b));
+	return 0;
 
 }
